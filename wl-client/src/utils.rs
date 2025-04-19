f693978fcef737ac85d4db88bc407b4c6898171f
Julian Orth <ju.orth@gmail.com>
2025-04-19 10:54:38 +0200

    Improve reporting of OS errors

--- wl-client/src/utils.rs
+++ wl-client/src/utils.rs
@@ -2,6 +2,7 @@ pub(crate) mod block_on;
 pub(crate) mod eventfd;
 pub(crate) mod executor;
 pub(crate) mod on_drop;
+pub(crate) mod os_error;
 pub(crate) mod poller;
 pub(crate) mod reentrant_mutex;
 pub(crate) mod sync_cell;
